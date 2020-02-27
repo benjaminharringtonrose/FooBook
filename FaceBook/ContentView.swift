ca2cb29de4d18516b661ba51d575f2675aeb09cc
Benjamin Rose <benjaminrose@Benjamins-MacBook-Air.local>
2020-02-26 20:59:30 -0500

    initial commit

---
+++ FaceBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FaceBook
+//
+//  Created by Benjamin Rose on 12/14/19.
+//  Copyright © 2019 Benjamin Rose. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
